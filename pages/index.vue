8be6306aeba10d2df861f910831c69b0131d408b
Reiko Makimoto <gracesystems2016@gmail.com>
2021-08-13 19:10:18 +0900

    最初のページ作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>hello world!!!</div>
+  <div>
+    <div v-for="home in homes" :key="home.objectID" style="float:left;margin:10px">
+      <img :src="home.images[0]" style="width:200px" alt="" /><br />
+      {{ home.title }} <br />
+      {{ home.location.address }} {{ home.location.city }} {{ home.location.state }} <br />
+      {{ home.guests }} guests <br />
+      {{ home.pricePerNight }} / night <br />
+    </div>
+  </div>
 </template>
+<script>
+import homes from "../data/homes";
+export default {
+  data() {
+    return {
+      homes: homes.slice(0, 3),
+    };
+  },
+};
+</script>
