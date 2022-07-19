ba68509ddf463b5080b79708b494af234fe3b1bb
makimoto <maki20020909@gmail.com>
2022-07-20 08:59:07 +0900

    lesson 19 - problem 1 - network latency

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -1,31 +1,18 @@
 <template>
-
   <div>
-
     <div style="display:flex;">
-
       <img v-for="image in home.images" :key="image" :src="image" width="200" height="150" />
-
     </div>
-     {{ home.title }}
-    <br />
-     ${{ home.pricePerNight }} / night
-    <br />
+    {{ home.title }}<br />
+    ${{ home.pricePerNight }} / night<br />
+    <img src="/images/marker.svg" width="20" height="20" />{{ home.location.address }} {{ home.location.city }} {{ home.location.state }}
+    {{ home.location.country }}<br />
 
-    <img src="/images/marker.svg" width="20" height="20" />
-     {{ home.location.address }} {{ home.location.city }} {{ home.location.state }} {{ home.location.country }}
-    <br />
-
-    <img src="/images/star.svg" width="20" height="20" />
-     {{ home.reviewValue }}
-    <br />
-     {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{ home.bathrooms }} bath
-    <br />
-     {{ home.description }}
+    <img src="/images/star.svg" width="20" height="20" />{{ home.reviewValue }}<br />
+    {{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{ home.bathrooms }} bath<br />
+    {{ home.description }}
     <div style="height:800px;width:800px" ref="map"></div>
-
   </div>
-
 </template>
 
 <script>
@@ -84,4 +71,3 @@ export default {
   },
 };
 </script>
-
